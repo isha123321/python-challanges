f8756572dcbd99210eb156f57e527a53014d27f5
isha123321 <isha101112@hotmail.com>
2024-10-29 21:20:15 +0500

    first commit

---
+++ a1.py
@@ -0,0 +1,14 @@
+speed=int(input("enter the cyclist no.28 speed"))
+run=int(input("enter the cyclist no.34 speed"))
+walk=int(input("enter cyclist no.54 speed"))
+
+sum=(speed+run+walk)
+average=(sum/3)
+if (speed>average):
+    print(speed,"is greater then average")
+if (run>average):
+    print(run,"is greater then average")   
+if(walk>average):
+    print(run,"is greater then average")    
+
+
---
+++ a2.py
@@ -0,0 +1,6 @@
+high=int(input("enter a biggrr number:"))
+low=int(input("enter a lower number"))
+if (high%low==0):
+    print("your value is divisible" )
+else:
+    print("no the value is not divisible")    
